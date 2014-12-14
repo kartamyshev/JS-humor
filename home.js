dc1519b882aa8222afcd3f3f4ad85b7a1f7da4eb
kartamyshev <kostya85>
2014-12-14 15:41:24 +0300

    adds dynamic data-x attrs to all steps

--- home.js
+++ home.js
@@ -1,36 +1,39 @@
-var $ = function(sel) {
-    return document.querySelector(sel);
-};
+var App = (function(){
 
-var $$ = function(sel) {
-    return document.querySelectorAll(sel);
-};
+    var $ = function(sel) { return document.querySelector(sel);},
+        $$ = function(sel) { return document.querySelectorAll(sel);},
+        stepsCount = $$('.step').length;
 
-var App = {
+    return {
 
-    ProgressInit: (function() {
-        var text = ' of ' + $$('.step').length;
+        setProgress: function () {
+            var text = ' of ' + stepsCount;
 
-        function insertText() {
-            $('.progress').textContent = location.hash.slice(2) + text;
-        }
+            return function insertText() {
+                $('.progress').textContent = location.hash.slice(2) + text;
+            }();
 
-        return function () {
-            return insertText();
-        };
-    }()),
+        },
 
-    Impress: (function() {
-        return function() {
+        impress: function () {
             return impress().init();
-        }
-    }()),
+        },
 
-    init: function() {
-        this.ProgressInit();
-        this.Impress();
-    }
-};
+        setDataXAttrs: function () {
+            var windowWidth = window.innerWidth;
+            for (var i = 1, j = 0; i <= stepsCount; i++, j++) {
+                $('.step:nth-child(' + i + ')').setAttribute('data-x', windowWidth * j);
+            }
+        },
+
+        init: function () {
+            this.setDataXAttrs();
+            this.setProgress();
+            this.impress();
+        }
+    };
+
+}());
 
 App.init();
-window.addEventListener('hashchange', App.ProgressInit);
+window.addEventListener('hashchange', App.setProgress, false);
